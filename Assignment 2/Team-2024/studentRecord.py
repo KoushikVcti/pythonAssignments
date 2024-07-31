729a8ef4ca1f79b2da036eed119669b055b37703
KoushikVcti <koushik.krishnappa@vcti.io>
2024-07-31 12:47:59 +0530

    added mainmenu

--- Assignment 2/Team-2024/studentRecord.py
+++ Assignment 2/Team-2024/studentRecord.py
@@ -23,5 +23,25 @@ class studentScore:
    
    
 
+def main_menu(self):
+        while True:
+            print("\nMain Menu")
+            print("1. Retrieve Student Score")
+            print("2. Store Student Score")
+            print("3. Calculate and Update Average Scores")
+            print("4. Exit")
+            choice = input("Enter your choice: ").strip()
 
+            if choice == '1':
+                rollno_to_search = input("Enter RollNo to search: ").strip()
+                result = self.RetriveStudentScore(rollno_to_search)
+                print(result)
+            elif choice == '2':
+                self.store_student_score()
+            elif choice == '3':
+                self.calculate_average_scores()
+            elif choice == '4':
+                break
+            else:
+                print("Invalid choice, please try again.")
 
